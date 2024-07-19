63df92100bd270e83f10afc5063c0b38a5e5e48c
ljs <452109014@qq.com>
2024-07-19 18:09:36 +0800

    2024/7/19

---
+++ base/validate.py
@@ -0,0 +1,9 @@
+from base.waitclick import *
+#验证列表内容
+def validate_list_content(wait, xpath, value):
+    can= wait_return_subelement_absolute(wait, time_w = 0.5, xpath=xpath)
+    char_amountof_member =can.get_attribute("value")
+    if char_amountof_member != value:
+        return False
+    elif char_amountof_member == value:
+        return True
