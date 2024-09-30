7858613db723083fd93e0332b4990d4f189e6d39
ljs <452109014@qq.com>
2024-09-30 17:05:17 +0800

    2024/9/30

--- app/transname.py
+++ app/transname.py
@@ -1,2 +1,50 @@
-student = []
+from base.myopenpyxl.xls2py import *
+
+coachs = []
+students = []
+students_gift = []
+coachs_pocket = []
+
+
+def goodboy(coachs, students, students_gift):
+    for index_c, coach in enumerate(coachs):
+
+        #获得每一个学生的得分放到students[index][0]里
+        for index, student in enumerate(students):
+            score = 0
+            sujects = student[0].split(",")
+            for suject in sujects:
+                if suject in coach:
+                    score += 1
+            students[index][1] = score
+
+        #初始化最大得分和最大得分的学生的序号
+        max_score = 0
+        max_score_student_index = 0
+
+        #查找最高的分的学生的得分和序号存放在上面的max_score、max_score_student_index里
+        for index, student in enumerate(students):
+            if students[index][1] > max_score:
+                max_score = students[index][1]
+                max_score_student_index = index
+
+        #判断最好的学生的得分，如果是0则'-'传入最终结果，否则学生的礼品交给教官
+        if max_score == 0:
+            coachs_pocket[index_c] = '-'
+        else:
+            coachs_pocket[index_c] = students_gift[max_score_student_index]
+
+
+coachs = xls2list("G:\\project\\LHSextract\\开发区参与考核企业名单（新版）.xlsx", 'B2', coachs)
+students = xls2list("G:\\project\\LHSextract\\企业关键字对应规范名.xlsx", 'A1', students)
+for index, student in enumerate(students):
+    students[index] = [student, 0]
+students_gift = xls2list("G:\\project\\LHSextract\\企业关键字对应规范名.xlsx", 'B1', students_gift)
+coachs_amount = len(coachs)
+coachs_pocket = [''] * coachs_amount
+goodboy(coachs, students, students_gift)
+print(coachs_pocket)
+
+
+
 
---
+++ base/myopenpyxl/xls2py.py
@@ -0,0 +1,26 @@
+import openpyxl
+
+
+#给定xls的路径，单元格，返回列的内容到列表
+def xls2list(workbookpath, str, list):
+    # 打开指定的 Excel 文件
+    workbook = openpyxl.load_workbook(workbookpath)
+    sheet = workbook.active  # 默认选择活动的工作表
+    # 指定单元格
+    #start_cell = 'A1'
+    start_cell = str
+    start_row = sheet[start_cell].row
+    list = []
+    letter = ''.join([char for char in start_cell if char.isalpha()])
+    number = char_to_number(letter)
+    #number = ''.join([char for char in start_cell if char.isdigit()])
+    # 遍历指定单元格下方的所有行
+    for row in sheet.iter_rows(min_row=start_row, min_col=int(number), max_col=int(number)):
+        for cell in row:
+            if cell.value is not None:  # 检查单元格是否有内容
+                list.append(cell.value)
+    return list
+
+
+def char_to_number(char):
+    return ord(char.lower()) - ord('a') + 1
